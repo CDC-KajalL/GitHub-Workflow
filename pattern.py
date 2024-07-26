b639a2f0e1ea54306748cad8aff15199a0c61fa9
Einsteinia11 <informationviakate2004@gmail.com>
2024-07-26 17:03:25 +0530

    Added some modifications

--- pattern.py
+++ pattern.py
@@ -1,10 +1,10 @@
 for i in range(0, 5):
     for j in range(0, i):
-        print("*", end = " ")
+        print("*", end = "")
     print("")
 n = 5
 for i in range(0, 5):
     for j in range(0, n):
-        print("*", end = " ")
+        print("*", end = "")
     print("")
     n-=1
